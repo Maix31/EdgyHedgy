9eef199c4b923fed06ee7196c8b9cfc861deefc3
Maix <darkmitko@abv.bg>
2021-06-08 02:26:23 +0300

    Add typecript support

--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,25 @@ import {Renderer} from "./renderer.js";
 import "../node_modules/gl-matrix/gl-matrix.js"
 // import { glMatrix } from "../node_modules/gl-matrix/gl-matrix.js";
 
-new Renderer().hello();
 
 // glMatrix.setMatrixArrayType(Array);
 
 // if (!glMatrix.equals(1,1)) {
-//   console.log("It works");
-// }
-
-//
-// start here
-//
-function main() {
+  //   console.log("It works");
+  // }
+  
+  //
+  // start here
+  //
+  function main() {
     const canvas = document.createElement("canvas");
     document.body.appendChild(canvas);
     // Initialize the GL context
     const gl = canvas.getContext("webgl2");
+    new Renderer(gl).hello();
   
+    console.log("Hello")
+
     // Only continue if WebGL is available and working
     if (gl === null) {
       alert("Unable to initialize WebGL. Your browser or machine may not support it.");
@@ -26,7 +28,7 @@ function main() {
     }
   
     // Set clear color to black, fully opaque
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clearColor(1.0, 1.0, 0.0, 1.0);
     // Clear the color buffer with specified clear color
     gl.clear(gl.COLOR_BUFFER_BIT);
 }
---
+++ src/renderer.ts
@@ -0,0 +1,13 @@
+class Renderer {
+
+    constructor(gl: WebGL2RenderingContext) {
+
+    }
+
+    hello() {
+        console.log("Hell");
+    }
+
+}
+
+export {Renderer}   ;
